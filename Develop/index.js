48c3c31a8886539d82592f7c3602921424fb98bd
ellysaschottler <ellysastork@gmail.com>
2023-11-03 17:27:45 -0500

    installed inquirer and linked it, wrote questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,72 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the what, why and how of your project.  What did you learn? What problem does this project solve?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide step by step directions to install your project:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Instruct how to use this project, include screenshots:'
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Type your screenshot file path:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'List the project contributors along with their GitHub profile links. List any third party assets or tutorials.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'If you have written tests for your application indicate how to run them here:'
+    },
+    {
+        type: 'input',
+        name: 'questionsEmail',
+        message: 'Type in your email address so that users can contact you with questions:'
+    },
+    {
+        type: 'input',
+        name: 'questionsGitHub',
+        message: 'Type in your GitHub profile link so that users can contact you with questions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Add a license:',
+        choices: 
+            [
+            "Apache License 2.0", 
+            "GNU General Public License v3.0", 
+            "MIT License", 
+            "BSD 2-Clause", 
+            "BSD 3-Clause", 
+            "Boost Software License 1.0", 
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse PUblic License 2.0"
+            ]
+    }
+];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
